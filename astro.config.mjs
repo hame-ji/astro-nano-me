b8824b1a2ae00e795e00b5478789a8a8dc1c8ff0
hame-ji <56395422+hame-ji@users.noreply.github.com>
2025-09-02 11:03:39 +0200

    fix(deployment): update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://ram-4.github.io",
+  site: "https://hame-ji.github.io",
   base: "/astro-nano-me",
   trailingSlash: "never",
   integrations: [mdx(), sitemap(), tailwind()],
