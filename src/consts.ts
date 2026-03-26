0ccfbf000a643172e876fd4be2361038aa0e6f26
hame-ji <56395422+hame-ji@users.noreply.github.com>
2026-03-26 17:19:28 +0100

    fix(site): update footer contact email address

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Site, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "hame-ji",
-  EMAIL: "contact@ram4.addy.io",
+  EMAIL: "contact@hameji.addy.io",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
